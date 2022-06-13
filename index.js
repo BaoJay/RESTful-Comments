b706ccab31e88073c6ebb552f156d2f8ae1a5f95
Gia Bao <giabao.nguyenhoang1001@gmail.com>
2022-06-13 15:31:35 +0700

    Add UUID feature

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+// Access things from NPM Modules
 const express = require("express");
 const app = express();
 const path = require("path");
+// Provide a new name for v4
+const { v4: uuid } = require("uuid");
 
 // app.use is executed everytime the request
 // is sent to the server
@@ -17,18 +20,22 @@ app.set("views", path.join(__dirname, "/views"));
 // Add comments data
 const comments = [
   {
+    id: uuid(),
     username: "Bao",
     comment: "Đây la Gia Bảo nè!!",
   },
   {
+    id: uuid(),
     username: "Truong",
     comment: "Truong Pham dep trai the!",
   },
   {
+    id: uuid(),
     username: "Thi",
     comment: "Anh Thi hoc nhanh qua!!",
   },
   {
+    id: uuid(),
     username: "Phuoc",
     comment: "UI gioi oi! Dong nghiep.",
   },
@@ -42,26 +49,40 @@ app.get("/comments/new", (req, res) => {
   res.render("comments/new");
 });
 
-// Create a POST route
+// Create a POST route =======================
 app.post("/comments", (req, res) => {
   console.log(req.body);
   // Phải destructuring object mới lấy data được
   const { username, comment } = req.body;
-  comments.push({ username, comment });
+  comments.push({ username, comment, id: uuid() });
   // res.send("Ngon lành!!!! It worked!!");
   // Redirect url and update the info
   res.redirect("/comments");
 });
 
-app.get("/comtam", (req, res) => {
-  res.send("GET /comtam respone!!");
+// Create a SHOW route ======================
+app.get("/comments/:id", (req, res) => {
+  // Destructuring data
+  const { id } = req.params;
+  // hàm find(): nếu trong array có phần tử nào giống thì lấy
+  // ra phần tử đầu tiên
+  const comment = comments.find((m) => m.id === id);
+  console.log(comment);
+  // res.send("được nè!");
+  // Redirect đến show.ejs
+  res.render("comments/show", { comment });
 });
 
-app.post("/comtam", (req, res) => {
-  console.log(req.body);
-  const { mon, sluong } = req.body;
-  res.send(`Ok, đây là đơn hàng của bạn: Món ${mon}, với số lượng ${sluong}`);
-});
+// // ====== Test comtam ==================
+// app.get("/comtam", (req, res) => {
+//   res.send("GET /comtam respone!!");
+// });
+
+// app.post("/comtam", (req, res) => {
+//   console.log(req.body);
+//   const { mon, sluong } = req.body;
+//   res.send(`Ok, đây là đơn hàng của bạn: Món ${mon}, với số lượng ${sluong}`);
+// });
 
 app.listen(3005, () => {
   console.log("Listening on post 3005 !!");
